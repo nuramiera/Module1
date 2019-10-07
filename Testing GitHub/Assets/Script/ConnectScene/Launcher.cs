883c3ce808724e2c80c0de4f84bdc14a701f04fb
nuramiera <nuramiera@users.github.com>
2019-10-07 10:58:32 +0800

    Import Module1 -  Front Inferface

---
+++ Testing GitHub/Assets/Script/ConnectScene/Launcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    
+    string roomName = "My Room";
+    bool ShowGUI = true;
+    
+
+    
+
+    public void OnClick_ConnectBtn()
+    {
+
+         PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+
+    public override void OnConnected()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        PhotonNetwork.LoadLevel("Rooms");
+
+    }
+}
---
+++ Testing GitHub/Assets/Script/UI/Rooms/LeaveRoomMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LeaveRoomMenu : MonoBehaviour
+{
+    private RoomCanvases _roomCanvas;
+    
+    public void FirstInitialize (RoomCanvases canvases)
+    {
+        _roomCanvas = canvases;
+    }
+
+    public void OnClick_LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom(true);
+        _roomCanvas.CurrentRoomCanvas.Hide();
+    }
+
+     
+}
